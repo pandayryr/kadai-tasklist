8562e1139575818bd9e735c13a3559e8ad687693
kouhei.azuse <knp.panda@gmail.com>
2019-12-23 08:17:02 +0000

    user signup,login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in, only: [:index, :show, :new, :edit]
+    before_action :correct_user, only: [:show, :edit, :update, :destroy]
     
     def index
         @tasks = Task.all
@@ -9,11 +11,11 @@ class TasksController < ApplicationController
     end
     
     def new
-        @task = Task.new
+        @task = current_user.tasks.build
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = 'Taskが正常に投稿されました'
@@ -53,5 +55,13 @@ class TasksController < ApplicationController
     #Strong Parameter
     def task_params
         params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        
+        unless @task
+            redirect_to '/'
+        end
     end
 end
